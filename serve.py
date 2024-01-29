6df63a259c9d61ba88ca3c631e811622d03b4020
Anna Liu <90288812+Annaa3@users.noreply.github.com>
2024-01-29 17:39:26 -0600

    Update serve.py

--- serve.py
+++ serve.py
@@ -5,15 +5,13 @@ import socket
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
-def handle_seed():
-    global curr_seed
-
+def serve():
     if request.method == 'POST':
         subprocess.Popen(['python3', 'stress_cpu.py'])
         return str('running stress')
 
     elif request.method == 'GET':
-        return str(socket.gethostname())
+        return str(socket.gethostbyname(hostname))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000)
