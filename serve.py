6a9db9057fce8a921de246c7ca918997e600d2b3
Anna Liu <90288812+Annaa3@users.noreply.github.com>
2024-01-29 12:37:33 +0800

    Update serve.py

--- serve.py
+++ serve.py
@@ -15,4 +15,4 @@ def handle_seed():
         return str(socket.gethostname())
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
